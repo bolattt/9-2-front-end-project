9000d276c434508c3ad0c7225fabe0181d7864fb
Bo Latt <bolattny@gmail.com>
2022-09-13 03:35:19 -0400

    add search input, more styling to navbar

--- script.js
+++ script.js
@@ -4,14 +4,38 @@ const header = {
     Authorization: "9a3c6ce90d4b4958a771bfc5370df6b1",
   },
 };
+const displayList = document.querySelector(".display-list");
 
 // fetch(BASE, header)
 //   .then((res) => res.json())
-//   .then((data) => console.log(data))
+//   .then((data) => {
+//     console.log(data);
+//     displayNews(data.articles);
+//   })
 //   .catch((err) => console.log(err));
 
-fetch("./db.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => console.log(data));
+function displayNews(articles) {
+  for (let article of articles) {
+    if (article.urlToImage) {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const img = document.createElement("img");
+      const p = document.createElement("p");
+
+      img.src = article.urlToImage;
+      a.href = article.url;
+      a.append(img);
+      a.setAttribute("target", "_blank");
+      p.textContent = article.title;
+      p.classList.add("news-description");
+      p.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+      });
+      li.classList.add("card");
+      li.append(a);
+      li.append(p);
+
+      displayList.append(li);
+    }
+  }
+}
